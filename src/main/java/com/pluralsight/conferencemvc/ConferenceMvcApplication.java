4a0a8df9ff8d02b82bc2597fe0f91674456ae71c
boris.milicevic <boris31094@gmail.com>
2021-05-24 09:29:30 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencemvc/ConferenceMvcApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceMvcApplication.class, args);
+    }
+
+}
