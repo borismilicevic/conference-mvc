aa5322dacf79b9cadbfaf572abd1451d004c33d9
boris.milicevic <boris31094@gmail.com>
2021-05-25 09:00:04 +0200

    Spring MVC fundamentals
    End of module 3

--- src/main/java/com/pluralsight/conferencemvc/ConferenceMvcApplication.java
+++ src/main/java/com/pluralsight/conferencemvc/ConferenceMvcApplication.java
@@ -2,9 +2,10 @@ package com.pluralsight.conferencemvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceMvcApplication {
+public class ConferenceMvcApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConferenceMvcApplication.class, args);
